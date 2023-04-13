9440ccaf947b7328111e23222fa580cb3d88237d
mayankjx <prototype.mayank@gmail.com>
2023-04-13 15:06:13 +0530

    created grid layout and added info cards basic css

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,16 +1,41 @@
 <template>
-  <h1>Dashboard</h1>
-  <div class="grid">
-    <div class="col-4 bg-orange">4</div>
-    <div class="col">1</div>
-    <div class="col">1</div>
-    <div class="col">1</div>
-    <div class="col">1</div>
-    <div class="col">1</div>
-    <div class="col">1</div>
-    <div class="col">1</div>
-    <div class="col">1</div>
+  <div class="container">
+    <h1>Dashboard</h1>
+    <div class="grid">
+      <div class="col-12 sm:col-3 bg-orange">
+        <InfoContainer></InfoContainer>
+      </div>
+      <div class="col-12 sm:col-3 bg-orange">
+        <InfoContainer></InfoContainer>
+      </div>
+      <div class="col-12 sm:col-3 bg-orange">
+        <InfoContainer></InfoContainer>
+      </div>
+    </div>
+    <div class="grid">
+      <div class="col-12"></div>
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import InfoContainer from "./InfoConatiner.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    InfoContainer,
+  },
+};
+</script>
+
+<style>
+.container {
+  /* text-align: center; */
+}
+
+.grid {
+  margin: auto;
+  text-align: center;
+}
+</style>
--- src/components/InfoConatiner.vue
+++ src/components/InfoConatiner.vue
@@ -1,3 +1,40 @@
-<template></template>
+<template>
+  <div class="info">
+    <div class="value">98</div>
+    <div class="title">Heart Rate</div>
+    <div class="remark">This heart rate is very good</div>
+  </div>
+</template>
 
-<script></script>
+<script>
+import Card from "primevue/card";
+
+export default {
+  name: "InfoContainer",
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style>
+.info {
+  /* height: 40vh; */
+  padding: 20px;
+  border-radius: 10px;
+  background-color: bisque;
+}
+
+.value {
+  font-size: 2.5em;
+}
+
+.title {
+  font-size: 1.2em;
+  padding-top: 3px;
+}
+
+.remark {
+  padding-top: 7px;
+}
+</style>
